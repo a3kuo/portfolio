86d0a627567e9718519f6d96618e669dce50ace2
Alexander Kuo <a3kuo@ucsd.edu>
2025-04-18 21:09:50 -0700

    added js to all files

---
+++ global.js
@@ -0,0 +1,75 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+// }
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/a3kuo', title: 'Profile'},
+    { url: 'resume/', title: 'Resume' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/"; 
+
+
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    // next step: create link and add it to nav
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+      
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select id="selector">
+              <option value="light dark">Automatic</option><option value="light">Light</option><option value="dark">Dark</option>
+          </select>
+      </label>`,
+  );
+
+let select = document.querySelector('#selector');
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value
+});
+
+if (("colorScheme") in localStorage) {
+    select.value = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', select.value);
+}
