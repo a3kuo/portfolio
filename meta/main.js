8e7a0455668c037ef3b3c1ff88a6969dc0ab5368
Alexander Kuo <a3kuo@ucsd.edu>
2025-05-31 22:29:21 -0700

    finished lab 8

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
 
 async function loadData() {
     const data = await d3.csv('loc.csv', (row) => ({
@@ -39,7 +40,7 @@ function processCommits(data) {
         });
   
         return ret;
-    });
+    }).sort((a, b) => a.datetime - b.datetime);
 }
 
 function renderCommitInfo(data, commits) {
@@ -355,6 +356,116 @@ function onTimeSliderChange() {
     filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
     updateScatterPlot(data, filteredCommits);
     updateCommitInfo(data, filteredCommits);
+    let lines = filteredCommits.flatMap((d) => d.lines);
+    let files = d3
+    .groups(lines, (d) => d.file)
+    .map(([name, lines]) => {
+        return { name, lines };
+    }) .sort((a, b) => b.lines.length - a.lines.length);
+    let filesContainer = d3
+        .select('#files')
+        .selectAll('div')
+        .data(files, (d) => d.name)
+        .join(
+            // This code only runs when the div is initially rendered
+            (enter) =>
+            enter.append('div').call((div) => {
+                div.append('dt').append('code');
+                div.append('dd');
+            }),
+        );
+
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    // This code updates the div info
+    // filesContainer.select('dt > code').text((d) => d.name);
+    // filesContainer.select('dd').text((d) => `${d.lines.length} lines`);
+    filesContainer.select('dt > code').html(d => `${d.name}<small>${d.lines.length} lines</small>`);
+    // append one div for each line
+    filesContainer
+    .select('dd')
+    .selectAll('div')
+    .data((d) => d.lines)
+    .join('div')
+    .attr('class', 'loc')
+    .attr('style', (d) => `--color: ${colors(d.type)}`);
 }
 timeSlider.addEventListener('input', onTimeSliderChange);
 onTimeSliderChange();
+
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a id="details" href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`,
+    );
+
+function onStepEnter(response) {
+    commitProgress = timeScale(response.element.__data__.datetime)
+    commitMaxTime = timeScale.invert(commitProgress);
+    const date = document.getElementById('slider-time')
+    date.textContent = commitMaxTime.toLocaleString('en', {
+        dateStyle: 'long', timeStyle: 'short'
+    });
+
+    filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
+    updateScatterPlot(data, filteredCommits);
+    updateCommitInfo(data, filteredCommits);
+    let lines = filteredCommits.flatMap((d) => d.lines);
+    let files = d3
+    .groups(lines, (d) => d.file)
+    .map(([name, lines]) => {
+        return { name, lines };
+    }) .sort((a, b) => b.lines.length - a.lines.length);
+    let filesContainer = d3
+        .select('#files')
+        .selectAll('div')
+        .data(files, (d) => d.name)
+        .join(
+            // This code only runs when the div is initially rendered
+            (enter) =>
+            enter.append('div').call((div) => {
+                div.append('dt').append('code');
+                div.append('dd');
+            }),
+        );
+
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    // This code updates the div info
+    // filesContainer.select('dt > code').text((d) => d.name);
+    // filesContainer.select('dd').text((d) => `${d.lines.length} lines`);
+    filesContainer.select('dt > code').html(d => `${d.name}<small>${d.lines.length} lines</small>`);
+    // append one div for each line
+    filesContainer
+    .select('dd')
+    .selectAll('div')
+    .data((d) => d.lines)
+    .join('div')
+    .attr('class', 'loc')
+    .attr('style', (d) => `--color: ${colors(d.type)}`);
+}
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
